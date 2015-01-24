cfe6a60b077e6f1514a90c2a2386241e2edcad55
yunmingzhang17 <zhangyunming1990@gmail.com>
2015-01-24 12:41:25 -0500

    adding the script for analyzing the separate directory

--- snapshot_scripts/analyze_snapshots3.m
+++ snapshot_scripts/analyze_snapshots3.m
@@ -3,11 +3,11 @@ close all
 
 
 addpath /data/vision/torralba/regionmem/memorability_cnn/lib/caffeLib;
-snapshotDir ='/data/vision/scratch/torralba/khosla/deep_train/imagenet_iter/';
-snapshotsNums = sort(importdata('/data/vision/scratch/torralba/khosla/cnn_dsl/caffe/snapshot_scripts/output_snapshot_num.txt'));
-
 %snapshotDir ='/data/vision/scratch/torralba/khosla/deep_train/imagenet_iter/';
 %snapshotsNums = sort(importdata('/data/vision/scratch/torralba/khosla/cnn_dsl/caffe/snapshot_scripts/output_snapshot_num.txt'));
+
+snapshotDir ='/data/vision/torralba/datasetbias/caffe-latest/examples/imagenet/';
+snapshotsNums = sort(importdata('/data/vision/scratch/torralba/khosla/cnn_dsl/caffe/snapshot_scripts/output_snapshot_num2.txt'));
 
 [s1, s2] = size(snapshotsNums);
 
@@ -15,18 +15,26 @@ numLayers = 8;
 numElem = s2;
 %stepSize = 4;
 meanVector = zeros(numLayers, numElem);
+sumAVector = zeros(numLayers, numElem);
 iterVector = zeros(1, numElem);
-
+iterVectorSum = zeros(1, numElem);
 c = caffeConfig(3);
 
 
 
 for i = 1 : numElem
-    snapshotFile = strcat(snapshotDir, 'caffenet_train_iter_', num2str(snapshotsNums(i)), '.caffemodel')
-    c.definition_file = '/data/vision/scratch/torralba/khosla/deep_train/imagenet_iter/deploy.prototxt';
+    snapshotFile = strcat(snapshotDir, 'caffe_object_train_iter_', num2str(snapshotsNums(i)));
+    %c.definition_file = '/data/vision/scratch/torralba/khosla/deep_train/imagenet_iter/deploy.prototxt';
+    c.definition_file = '/data/vision/torralba/datasetbias/caffe-latest/examples/imagenet/object_deploy.prototxt';
     c.binary_file  = snapshotFile;
     c = caffeInitialize(c);
-    
+    w = caffe('get_weights');
+    for j = 1: numLayers
+        A = abs(w(j).weights{1});
+        sumA = sum(A(:));
+        sumAVector(j, i) = sumA;    
+    end
+    iterVectorSum(i) = snapshotsNums(i);
     
     if (i == 1)
         lastw= caffe('get_weights');
@@ -41,7 +49,8 @@ for i = 1 : numElem
             diff = diff/meanVal; 
             meanDiff = mean(diff(:));
             %meanDiff = mean(mean(mean(mean(diff))));
-            meanVector(j, i) = meanDiff*100;    
+            meanVector(j, i) = meanDiff*100;            
+                       
         end
         lastw = currentw;
     end
@@ -52,7 +61,20 @@ for i = 1: numLayers
     h = figure;
     plot(iterVector, meanVector(i,:)');
     ylim([0,100]);
-    filename = strcat('layer', num2str(i));
+    filename = strcat('weight-change-percent-layer', num2str(i));
+    title(filename);
+    xlabel('iterations');
+    ylabel('change in percentage');
+    print(h, '-djpeg', filename);
+end
+
+for i = 1: numLayers
+    h = figure;
+    plot(iterVectorSum, sumAVector(i,:)');
+    filename = strcat('weight-abs-layer', num2str(i));
+    title(filename);
+    xlabel('iterations');
+    ylabel('absolute sum of weight');
     print(h, '-djpeg', filename);
 end
 
--- snapshot_scripts/parseSnapshots.py
+++ snapshot_scripts/parseSnapshots.py
@@ -5,9 +5,9 @@ import re
 
 #The script go through all the caffe model files and generate a file with all the iteration numbers
 
-def analyzeDir(snapshotDir):
+def analyzeDir(snapshotDir, output):
 
-    outputFile = open('output_snapshot_num.txt', 'w')
+    outputFile = open(output, 'w')
 
     snapshotList = os.listdir(snapshotDir) 
     for snapshot in snapshotList:
@@ -16,7 +16,22 @@ def analyzeDir(snapshotDir):
             snapshotNum = re.findall(r'\d+', snapshot)[0]
             #print snapshotNum
             outputFile.write(snapshotNum + ' ')
+    outputFile.close()
+
+def analyzeDir2(snapshotDir, output):
+
+    outputFile = open(output, 'w')
+
+    snapshotList = os.listdir(snapshotDir) 
+    for snapshot in snapshotList:
+        if snapshot.endswith('.solverstate') and snapshot.startswith('caffe_object_train_iter_'):
+            #print snapshot
+            snapshotNum = re.findall(r'\d+', snapshot)[0]
+            #print snapshotNum
+            outputFile.write(snapshotNum + ' ')
+    outputFile.close()
 
 
+analyzeDir('/data/vision/scratch/torralba/khosla/deep_train/imagenet_iter/', 'output_snapshot_num.txt')
 
-analyzeDir('/data/vision/scratch/torralba/khosla/deep_train/imagenet_iter/')
+analyzeDir2('/data/vision/torralba/datasetbias/caffe-latest/examples/imagenet', 'output_snapshot_num2.txt')
