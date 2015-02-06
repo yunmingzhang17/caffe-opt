6a602676ced735675a814e79628ce3d804902423
yunmingzhang17 <zhangyunming1990@gmail.com>
2015-02-05 19:59:54 -0500

    a working version that prints out weights of layers in the snapshots

--- scripts/analyze/caffe_features_imagelist2.m
+++ scripts/analyze/caffe_features_imagelist2.m
@@ -3,20 +3,38 @@ tmp1 = load('/data/vision/torralba/gigaSUN/deeplearning/dataset/feature_cafferef
 tmp2 = load('/data/vision/torralba/gigaSUN/deeplearning/dataset/feature_caffereference/DLfeature_ilsvrc2012test_pool5_giga.mat', 'imageList');
 image_list = [tmp1.imageList; tmp2.imageList;];
 clear tmp1 tmp2;
-numTest = 1000; 
+numTest = 2; 
 filelist = image_list(randperm(length(image_list), min(numTest, length(image_list))));
-c = caffeConfig(3);
-c.center_only = 1;
-c.reshape_features = 1;
+
 %layers = {'conv1', 'conv2', 'conv3', 'conv4', 'conv5', 'fc6', 'fc7'};
-layers = {'pool1', 'norm1', 'pool2', 'norm2', 'conv3'};
-dataset = tempname; dataset = dataset(5:15);
-features= caffeFeatures(dataset, filelist, layers, c);
-f = caffeLoad(dataset, layers, c);  %cell array, each being a cell of 2x4096
+layers = {'pool1', 'norm1', 'pool2', 'norm2', 'conv3', 'conv4', 'pool5', 'fc6', 'fc7'};
 
-for i=1:size(f,1)
+
+snapshotDir ='/data/vision/torralba/datasetbias/caffe-latest/examples/imagenet/';
+snapshotsNums = sort(importdata('/data/vision/scratch/torralba/khosla/cnn_dsl/caffe/snapshot_scripts/output_snapshot_num2.txt'));
+[s1, s2] = size(snapshotsNums);
+numElem = s2;
+iterVector = zeros(1, numElem);
+
+
+for i = 1 : numElem
+  snapshotFile = strcat(snapshotDir, 'caffe_object_train_iter_', num2str(snapshotsNums(i)))
+  c = caffeConfig(3);
+  c.definition_file = '/data/vision/torralba/datasetbias/caffe-latest/examples/imagenet/object_deploy.prototxt';
+  c.center_only = 1;
+  c.reshape_features = 1;
+  c.binary_file = snapshotFile;
+
+  dataset = tempname; dataset = dataset(5:15); %generating a tmp name
+  features= caffeFeatures(dataset, filelist, layers, c);
+  f = caffeLoad(dataset, layers, c);  
+
+  disp(['snapshot '  num2str(snapshotsNums(i))]);
+  for i=1:size(f,1)
 	f1 = f{i, 1};
-disp(['layer: ' layers{i} ', sparsity: ' num2str(mean(f1(:)==0))]);
-end
+  disp(['layer: ' layers{i} ', sparsity: ' num2str(mean(f1(:)==0))]);
 
+
+  end
+end
 
